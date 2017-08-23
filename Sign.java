65a74c8769c5cf323037019b6624e828b19f908c
nareshkumarnb <hseran16@gmail.com>
2017-08-23 22:06:22 +0530

    sign

---
+++ Sign.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Sign{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int no = sc.nextInt();
+		if (no > 0) {
+			System.out.println("53");
+		}
+		else if (no < 0) {
+			System.out.println("0");
+		}
+	}
+}
